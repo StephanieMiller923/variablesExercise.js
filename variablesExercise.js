838659ebed6e8f050c52d9d4d15a1dc2b3bbc856
Stephanie Miller <stephanie.miller923@gmail.com>
2021-02-23 12:45:38 -0600

    Edited variablesExercise.js

--- variablesExercise.js
+++ variablesExercise.js
@@ -21,3 +21,11 @@ console.log(blank);
 
 blank = null;
 console.log(blank);
+
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+console.log(quarter, dime, nickel, penny);
+
+let letters = "qwertyuiop";
+
+console.log(letters[letters.length - 1]);
